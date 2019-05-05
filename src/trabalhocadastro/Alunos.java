2ce80feb52139c5859cb1bd05935b476366d3e4e
victormorais07 <victormorais08@hotmail.com>
2019-05-05 19:08:20 -0300

    teste

---
+++ src/trabalhocadastro/Alunos.java
@@ -0,0 +1,19 @@
+
+package trabalhocadastro;
+
+
+public class Alunos {
+    
+    String nome;
+    int codigo;
+    String email;
+    String cidade;
+    String uf;
+    Alunos(String nome, int codigo,String email, String cidade,String uf){
+    this.nome=nome;
+    this.codigo=codigo;
+    this.email=email;
+    this.cidade=cidade;
+    this.uf=uf;
+}
+}
